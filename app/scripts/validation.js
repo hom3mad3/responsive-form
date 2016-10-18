04b2ee08d0dd33447adbcc46ae6e7fcea39f714c
hom3mad3 <micaelarava@gmail.com>
2016-10-18 17:05:04 +0200

    readme

--- app/scripts/validation.js
+++ app/scripts/validation.js
@@ -1,6 +1,6 @@
 $(function() {
 
-//custom error messages
+  //custom error messages
   $.validator.setDefaults({
     errorClass: 'help-block',
     highlight: function(element) {
@@ -22,17 +22,20 @@ $(function() {
     }
   });
 
-//safe password custom method
-$.validator.addMethod('strongPassword', function(value, element){
-  return this.optional(element) || //checks to see if input is required - if not,
-  value.length >=6 //returns true if password is longer than 6,
-  && /\d/.test(value)//has at least one digit,
-  && /[a-z]/i.test(value); //at least one character
-}, 'Password must be at least 6 characters long and contain one number');
+  //safe password custom method
+  $.validator.addMethod('strongPassword', function(value, element) {
+    return this.optional(element) || //checks to see if input is required - if not,
+      value.length >= 6 //returns true if password is longer than 6,
+      &&
+      /\d/.test(value) //has at least one digit,
+      &&
+      /[a-z]/i.test(value); //at least one character
+  }, 'Das Passwort muss mindestens 6 Zeichen lang sein und eine Ziffer enthalten');
 
 
-//validation
-  $("form").validate({
+  //validation
+  $('form').validate({
+    errorElement: 'div',
     rules: {
       email: {
         required: true,
@@ -40,10 +43,13 @@ $.validator.addMethod('strongPassword', function(value, element){
       },
       vorname: {
         required: true,
-        lettersonly: true
+        lettersonly: true,
+        nowhitespace: true
       },
       nachname: {
-        required: true
+        required: true,
+        lettersonly: true,
+        nowhitespace: true
       },
       mitglied: {
         required: true
@@ -53,13 +59,31 @@ $.validator.addMethod('strongPassword', function(value, element){
         strongPassword: true
       },
       checkbox: {
-        required:true
+        required: true
       }
     },
     messages: {
       email: {
-        required: 'ungültige email',
-        email: 'bla'
+        required: 'muss ausgefüllt werden',
+        email: 'ungültige email'
+      },
+      vorname: {
+        required: 'muss ausgefüllt werden',
+        lettersonly: 'ungültige information'
+      },
+      nachname: {
+        required: 'muss ausgefüllt werden',
+        lettersonly: 'ungültige information'
+      },
+      mitglied: {
+        required: 'muss ausgefüllt werden',
+        lettersonly: 'ungültige information'
+      },
+      password: {
+        required: 'muss ausgefüllt werden'
+      },
+      checkbox: {
+        required: 'muss ausgefüllt werden'
       }
     }
   });
--- test/spec/test.js
+++ test/spec/test.js
@@ -3,7 +3,7 @@
 
   describe('form', function() {
     var formElem = document.forms[0];
-    var registerButton = document.getElementById('signup-button');
+    var registerButton = document.getElementById('register-button');
 
     it('Form exists in the DOM', function() {
       expect(formElem).to.not.equal(null);
@@ -13,12 +13,25 @@
       expect(formElem.email.getAttribute('type')).to.equal('email');
     });
 
-    it('Register button has the right text', function() {
+    it('Input field should be set to type password', function() {
+      expect(formElem.password.getAttribute('type')).to.equal('password');
+    });
+
+    it('Register button has the right content', function() {
       expect(registerButton.innerHTML).to.equal('Jetzt Registrieren');
     });
   });
 
-  describe('form validation', function() {
-    
+  describe('user form', function() {
+
+      // critical
+      it('ensures invalid email addresses wont pass', function() {});
+      it('ensure valid email addresses pass validation', function() {});
+      it('ensure submitting form changes path', function() { });
+
+      // nice-to-haves
+      it('ensure client-side helper shown for empty fields', function() { });
+      it('ensure hitting enter on password field submits form', function() { });
+
   });
 })();
